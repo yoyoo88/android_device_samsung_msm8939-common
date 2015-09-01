ce687996b0dc09d927d20b2fadc8d34375c781cf
Mayank Rana <mrana@codeaurora.org>
2015-09-01 14:10:12 -0700

    msm8939: rootdir: Add USB compositions for MIDI functionality
    Add new USB compositions to support MIDI functionality.
    Add the following PIDs:
    0x90BA - MIDI
    0x90BB - MIDI + ADB
    
    Change-Id: I2f50a6b55c12fd7706644c6497d00f8a0c1c02fa
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- msm8939.mk
+++ msm8939.mk
@@ -40,6 +40,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # Audio
