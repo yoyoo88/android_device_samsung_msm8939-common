bf3dc00b084c076b4925ff2c9079017ff1a55402
Jackeagle <jackeagle102@gmail.com>
2016-03-16 05:14:05 -0400

    msm8939: Switch to OpenSource GPS HAL
    Change-Id: I0cc8e1136a36dfac533445d625d258045bc7973d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,3 +153,7 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/samsung/msm8939-common/sepolicy
+
+# GPS
+TARGET_GPS_HAL_PATH := device/samsung/a5-common/gps
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
---
+++ gps/Android.mk
@@ -0,0 +1,28 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
+
+ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
+
+ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
+    #For msm8960/apq8064 targets
+    include $(call all-named-subdir-makefiles,msm8960)
+else ifneq ($(filter msm8974 ,$(TARGET_BOARD_PLATFORM)),)
+    #For msm8974 target
+    include $(call all-named-subdir-makefiles,msm8974)
+else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
+    #For msm8084 target
+    include $(call all-named-subdir-makefiles,msm8084)
+else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
+    #For all other targets
+    GPS_DIRS=core utils loc_api platform_lib_abstractions etc
+    include $(call all-named-subdir-makefiles,$(GPS_DIRS))
+endif #TARGET_BOARD_PLATFORM
+
+else ifneq ($(filter msm8909 ,$(TARGET_BOARD_PLATFORM)),)
+    #For msm8909 target
+    include $(call all-named-subdir-makefiles,msm8909)
+else ifeq ($(filter msm8916 ,$(TARGET_BOARD_PLATFORM)),)
+    GPS_DIRS=core utils loc_api platform_lib_abstractions etc
+    include $(call all-named-subdir-makefiles,$(GPS_DIRS))
+endif #BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
+
+endif
